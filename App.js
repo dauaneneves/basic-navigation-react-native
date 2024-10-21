fdf56c63cf0b2c3ced01a85e1ac4852a80dd6915
Dauane Neves Geronimo <dauane.395414@alunosatc.edu.br>
2024-10-21 20:19:53 -0300

    Em sala

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from "react-navigation";
+import Home from "./Home";
+import Contact from "./Contact";
+
+const Stack = createNativeStackNavigator();
+const navigate = useNavigation();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+      <Stack.Navigator>
       <StatusBar style="auto" />
     </View>
   );
